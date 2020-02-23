3c7326654d17d7d1163355c0aa4d8004af927d9c
Rob P <robpeterman573@gmail.com>
2020-02-23 00:01:20 -0500

    Added function to console log WFB data stats and to check if keys exist within data

--- wfb-update-helpers.js
+++ wfb-update-helpers.js
@@ -7,6 +7,7 @@ exports.updateCountries = () => {
       "https://raw.githubusercontent.com/iancoleman/cia_world_factbook_api/master/data/factbook.json"
     )
     .then(res => {
+      logDataStats(res.data.countries);
       for (let country in res.data.countries) {
         Country.findOneAndUpdate(
           { name: res.data.countries[country].data.name },
@@ -26,3 +27,45 @@ exports.updateCountries = () => {
       }
     });
 };
+
+// console logs some basic stats about the WFB data
+function logDataStats(countries) {
+  let total = { countryCount: 0 };
+  for (let country in countries) {
+    total.countryCount++;
+    for (let key in countries[country].data) {
+      if (keyExists(key)) {
+        total[key] ? total[key]++ : (total[key] = 1);
+      }
+    }
+  }
+  console.log("*************************************");
+  for (let totalKey in total) {
+    if (totalKey === "countryCount") {
+      console.log(`${totalKey}: ${total[totalKey]}`);
+    } else {
+      console.log(
+        `${totalKey}: ${((total[totalKey] / 259) * 100).toFixed(2) + "%"}`
+      );
+    }
+  }
+  console.log("*************************************");
+}
+
+const mainKeys = [
+  "name",
+  "introduction",
+  "geography",
+  "people",
+  "government",
+  "economy",
+  "energy",
+  "communications",
+  "transportation",
+  "military_and_security",
+  "transnational_issues"
+];
+
+function keyExists(key) {
+  return mainKeys.includes(key);
+}
